d736ba34eedbb964630f1dd021ccd8602e95bfa3
Sankalp <sankalppawar619@gmail.com>
2025-12-29 02:30:38 +0530

    not log if not token

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Body from "./Body"
+import Feed from "./Feed"
+import Login from "./Login"
+import Profile from "./Profile"
+import { Provider } from "react-redux"
+import appStore from "./utils/appStore"
 
 function App() {
 
   return (
     <>
-      <h1>Hello</h1>
+      <Provider store={appStore}>
+        <BrowserRouter basename="/">
+          <Routes>
+            <Route path="/" element={<Body />} >
+              <Route path="/" element={<Feed />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/profile" element={<Profile />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     </>
   )
 }
---
+++ src/Login.jsx
@@ -0,0 +1,45 @@
+import { use, useState } from 'react'
+import axios from 'axios';
+import { addUser } from './utils/userSlice';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { BASE_URL } from './utils/constant';
+
+const Login = () => {
+  const [emailId, setEmailId] = useState("sank@gmail.com");
+  const [password, setPassword] = useState("Sankalp@123");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogin = async () => {
+
+    try {
+      const res = await axios.post(BASE_URL + "/login", {
+        emailId,
+        password,
+      }, { withCredentials: true });
+      dispatch(addUser(res.data));
+      navigate("/");
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <div className='flex justify-center'>
+      <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+        <legend className="fieldset-legend">Login</legend>
+
+        <label className="label">Email</label>
+        <input type="email" value={emailId} onChange={(e) => setEmailId(e.target.value)} className="input" placeholder="Email" />
+
+        <label className="label">Password</label>
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="input" placeholder="Password" />
+
+        <button className="btn btn-neutral mt-4" onClick={handleLogin}>Login</button>
+      </fieldset>
+    </div>
+  )
+}
+
+export default Login
